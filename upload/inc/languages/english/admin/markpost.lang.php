0dfc264dc6db0c16b44baef2fc38df5e73bc53aa
Laird <lairdshaw77@gmail.com>
2021-04-09 17:08:45 +1000

    Tweak the language files a little

--- upload/inc/languages/english/admin/markpost.lang.php
+++ upload/inc/languages/english/admin/markpost.lang.php
@@ -1,4 +1,4 @@
 <?php
 
 $l['markpost_uninstall'] = "Uninstall Markpost";
-$l['markpost_uninstall_message'] = "Are you sure you want to delete all the existing data of Mark Post?<br>If you choose 'No' the acumulated data will remain in database for future reference / usage.<br>Choosing 'Yes' will drop the table wiping all data and this can not be undone.";
+$l['markpost_uninstall_message'] = "Are you sure you want to delete all the existing data of Mark Post?<br>If you choose 'No' the accumulated data will remain in the database for future reference / usage.<br>Choosing 'Yes' will drop the table wiping all data and this can not be undone.";
